431ae9dbd280f9428dda8a7ea770ca41dea10309
rachna <pichreamr@gmail.com>
2026-02-26 00:02:38 +0700

    feat[#44]: Create credit screen with navigation and styling

--- quiz-game/src/routes/+layout.svelte
+++ quiz-game/src/routes/+layout.svelte
@@ -5,5 +5,5 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href="/favicon.png" /></svelte:head>
+<svelte:head><link rel="icon" href="" /></svelte:head>
 {@render children()}
