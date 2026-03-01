43ef66ae2df2c12d0483c73cbf426c2ecf8ab8df
Rachna <pichreamr@gmail.com>
2026-03-01 20:34:45 +0700

    Add X-content-type-options, refferer-policy, content-security-policy

---
+++ quiz-game/src/hooks.server.ts
@@ -0,0 +1,20 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+    const response = await resolve(event);
+	response.headers.set('X-Content-Type-Options', 'nosniff');
+    response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
+    response.headers.set(
+		'Content-Security-Policy',
+		"default-src 'self'; " +
+			"script-src 'self'; " +
+			"style-src 'self' 'unsafe-inline'; " +
+			"img-src 'self' data: https:; " +
+			"font-src 'self' data:; " +
+			"connect-src 'self'; " +
+			"frame-ancestors 'none'; " +
+			"base-uri 'self'; " +
+			"form-action 'self'"
+	);
+	return response;
+};
